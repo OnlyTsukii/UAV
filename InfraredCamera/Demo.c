2719dffe3818bec570c79fd9b1e41a553e6e6b92
OnlyTsukii <1431297183@qq.com>
2024-04-28 16:46:56 +0800

    fix bug

--- InfraredCamera/Demo.c
+++ InfraredCamera/Demo.c
@@ -115,7 +115,7 @@ void socket_serve()
 
         if (strcmp(buffer, CMD_SHOOT) == 0)
         {
-            shutter = false;
+            shutter = true;
         }
 
         // int response = 0;
