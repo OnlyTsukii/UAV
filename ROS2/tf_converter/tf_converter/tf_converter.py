77b86bf8740fc1f4e24998ba88d351af74d7448e
OnlyTsukii <1431297183@qq.com>
2024-05-01 22:07:57 +0800

    update package tf_converter

--- ROS2/tf_converter/tf_converter/tf_converter.py
+++ ROS2/tf_converter/tf_converter/tf_converter.py
@@ -3,10 +3,86 @@ import math
 
 from rclpy.node import Node
 from nmea_msgs.msg import DefectBox, GpsFix
+from typing import Tuple
 
+
+EARTH_RADIUS = 6378.137
 
 class TF_Converter(Node):
     def __init__(self):
         super().__init__("tf_converter")
+
         self.dft_subscriber = self.create_subscription(DefectBox, "dft_box", self.dft_callback, 10)
         self.gps_subscriber = self.create_subscription(GpsFix, "fix", self.gps_callback, 10)
+
+        self.map = {}
+        
+        self.init_longitude = 0
+        self.init_latitude = 0
+        self.init_altitude = 0
+
+    def gps_callback(self, msg: GpsFix):
+        pass
+
+    def dft_callback(self, msg: DefectBox):
+        pass
+
+    def handle_map(self, gps_msg: GpsFix, dft_msg: DefectBox):
+        # ToDo
+        pass
+
+    def rad(self, d):
+        return d * math.pi / 180.0
+    
+    def get_transform(self, msg: GpsFix) -> Tuple[float, float, float]:
+        if self.init_longitude == 0 and self.init_altitude == 0 \
+            and self.init_altitude == 0:
+            self.init_longitude = msg.longitude
+            self.init_latitude = msg.latitude
+            self.init_altitude = msg.altitude
+
+            return 0, 0, 0
+        else:
+            radLat1 = self.rad(self.init_latitude)
+            radLong1 = self.rad(self.init_longitude)
+            radLat2 = self.rad(msg.latitude)
+            radLong2 = self.rad(msg.longitude)
+            
+            delta_lat = radLat2 - radLat1
+            delta_long = 0
+            
+            if delta_lat > 0:
+                x = -2 * math.asin(math.sqrt(
+                    math.pow(math.sin(delta_lat / 2), 2) +
+                    math.cos(radLat1) * math.cos(radLat2) *
+                    math.pow(math.sin(delta_long / 2), 2)
+                ))
+            else:
+                x = -2 * math.asin(math.sqrt(
+                    math.pow(math.sin(delta_lat / 2), 2) +
+                    math.cos(radLat1) * math.cos(radLat2) *
+                    math.pow(math.sin(delta_long / 2), 2)
+                ))
+            x *= EARTH_RADIUS * 1000
+            
+            delta_lat = 0
+            delta_long = radLong2 - radLong1
+            if delta_long > 0:
+                y = 2 * math.asin(math.sqrt(
+                    math.pow(math.sin(delta_lat / 2), 2) +
+                    math.cos(radLat2) * math.cos(radLat2) *
+                    math.pow(math.sin(delta_long / 2), 2)
+                ))
+            else:
+                y = -2 * math.asin(math.sqrt(
+                    math.pow(math.sin(delta_lat / 2), 2) +
+                    math.cos(radLat2) * math.cos(radLat2) *
+                    math.pow(math.sin(delta_long / 2), 2)
+                ))
+            y *= EARTH_RADIUS * 1000
+            
+            z = msg.altitude - self.init_altitude
+
+            return x, y, z
+            
+    
---
+++ ROS2/tf_converter/tf_converter/uav2cam_broadcaster.py
@@ -0,0 +1,71 @@
+import rclpy
+import math
+import sys
+import numpy as np
+
+from geometry_msgs.msg import TransformStamped
+from rclpy.node import Node
+from tf2_ros.static_transform_broadcaster import StaticTransformBroadcaster
+
+
+def quaternion_from_euler(ai, aj, ak):
+    ai /= 2.0
+    aj /= 2.0
+    ak /= 2.0
+    ci = math.cos(ai)
+    si = math.sin(ai)
+    cj = math.cos(aj)
+    sj = math.sin(aj)
+    ck = math.cos(ak)
+    sk = math.sin(ak)
+    cc = ci*ck
+    cs = ci*sk
+    sc = si*ck
+    ss = si*sk
+
+    q = np.empty((4, ))
+    q[0] = cj*sc - sj*cs
+    q[1] = cj*ss + sj*cc
+    q[2] = cj*cs - sj*sc
+    q[3] = cj*cc + sj*ss
+
+    return q
+
+
+class StaticFramePublisher(Node):
+
+    def __init__(self):
+        super().__init__('uav_camera_broadcaster')
+
+        self.tf_static_broadcaster = StaticTransformBroadcaster(self)
+        self.publish_transforms()
+
+    def publish_transforms(self):
+        t = TransformStamped()
+
+        t.header.stamp = self.get_clock().now().to_msg()
+        t.header.frame_id = 'uav'
+        t.child_frame_id = 'camera'
+
+        t.transform.translation.x = 0
+        t.transform.translation.y = 0.25
+        t.transform.translation.z = -0.2
+        
+        t.transform.rotation.x = 0
+        t.transform.rotation.y = 0
+        t.transform.rotation.z = 0
+        t.transform.rotation.w = 1
+
+        self.tf_static_broadcaster.sendTransform(t)
+
+
+def main():
+    rclpy.init()
+    node = StaticFramePublisher()
+
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+
+    rclpy.shutdown()
